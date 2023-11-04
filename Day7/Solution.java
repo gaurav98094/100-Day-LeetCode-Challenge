5d77328fbbae4ac9beab63a2aa8f09cb74f82bb4
gauravkandel <kandelgaurav7@gmail.com>
2023-11-04 19:04:51 +0545

    1503. Last Moment Before All Ants Fall Out of a Plank

---
+++ Day7/Solution.java
@@ -0,0 +1,16 @@
+package Day7;
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int ans = 0;
+        for (int num : left) {
+            ans = Math.max(ans, num);
+        }
+
+        for (int num : right) {
+            ans = Math.max(ans, n - num);
+        }
+
+        return ans;
+    }
+}
